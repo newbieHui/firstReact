a55d84699ef5291d357822586ff727909e53c2ca
DemoHui <shaozh@yingkelecai.com>
2019-03-20 09:34:29 +0800

    1903200935

--- src/pages/Page1/Page1.js
+++ src/pages/Page1/Page1.js
@@ -8,7 +8,7 @@ export default class Page1 extends Component {
     render() {
         return (
             <div className={style.box}>
-                this is page1~
+                这里是首页
                 <img src={image}/>
             </div>
         )
